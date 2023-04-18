1927979dceac84c67a8337d049af8fb89cf0470b
Leibenspergera@moravian.edu <leibenspergera@moravian.edu>
2023-04-18 10:37:02 -0400

    helpppp

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def read_count():
     try:
         with open('data/count.txt', 'r') as f:
             count = int(f.read())
-        return count
+        return count:
     except IOError:
         return 0
 
@@ -27,7 +27,6 @@ def hello():
     count += 1
     save_count(count)
     return "<h1 style='color:red'>Hello World! {}</h1>".format(count)
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)

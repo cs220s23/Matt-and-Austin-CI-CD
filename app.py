a01daa28d550a3e50026c37136a7794abcc0a432
Matthew Simcoe <125500632+msimcoe345@users.noreply.github.com>
2023-04-24 20:57:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,4 +28,4 @@ def hello():
     return "<h1 style='color:red'>Hello World! {}</h1>".format(count)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)

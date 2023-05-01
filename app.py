5546125769b0a5c96bb5dea2b93ae1251e87c8d1
Austin Leibensperger <125526351+Waltzi@users.noreply.github.com>
2023-05-01 11:01:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,8 @@ r = redis.Redis(host=host, port=port)
 app = Flask(__name__)
 
 def read_count():
-    int(r.get('count'))
+    count=int(r.get('count'))
+    return count
 
 
 def save_count(count):
